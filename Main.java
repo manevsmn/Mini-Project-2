82e58d71bdc30b2127eb0619fe91d53f323277ea
manevsmn <manevsmn@mail.uc.edu>
2021-03-15 21:00:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,87 @@
+/*
+Mini Project 2
+Group #1: Martin Manevski, Nicholas Warren, Jamie Schaffer, Mariam Bahassi
+Date: 3/14/2021
+*/
+
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("How many players are in this Easter egg hunt? Enter a positive whole number:");
+    int num = s.nextInt();
+    while (num < 0) {
+    Scanner n = new Scanner(System.in);
+    System.out.println("This number is negative, please enter a new number:");
+    num = n.nextInt();
+    }
+
+    ArrayList<Player> players = new ArrayList<Player>();
+
+    for(int i=0; i<num; i++) {
+      players.add(new Player());
+    }
+
+    System.out.println("We have added " + num + " players to this hunt. Let's fund some eggs!");
+
+    hunt(players);
+
+    recap(players);
+
+    stats(players);
+    }
+
+    public static void hunt(ArrayList<Player> thePlayers) {
+      Random r = new Random();
+      int eggsFound;
+
+      for(int i=0; i<thePlayers.size(); i++) {
+        eggsFound = r.nextInt(11);
+        for(int b=0; b<eggsFound; b++) {
+          thePlayers.get(i).foundEgg(new Egg());
+        }
+      }
+    }
+
+    public static void recap(ArrayList<Player> thePlayers) {
+      for(int i=0; i<thePlayers.size(); i++) {
+        System.out.println("\nPlayer " + i + " found " + thePlayers.get(i).getNumberEggs() + " eggs");
+        thePlayers.get(i).printBasket();
+      }
+    }
+
+    public static void stats(ArrayList<Player> thePlayers) {
+      int mostEggsPlayer = 0;
+      int mostEggsNumber = 0;
+
+      for(int i=0; i<thePlayers.size(); i++) {
+        if(thePlayers.get(i).getNumberEggs() > mostEggsNumber) {
+          mostEggsNumber = thePlayers.get(i).getNumberEggs();
+          mostEggsPlayer = i;
+        }
+      }
+
+      System.out.println("\nPlayer " + mostEggsPlayer + " found the most eggs, with " + mostEggsNumber + " eggs!");
+
+      int[] colors = new int [4];
+      for(int i=0; i<thePlayers.size(); i++) {
+        for(int b=0; b<thePlayers.get(i).getNumberEggs(); b++){
+           if(thePlayers.get(i).getBasket().get(b).getColor().equals("blue"))
+            colors[0]++;  
+          if(thePlayers.get(i).getBasket().get(b).getColor().equals("pink"))
+            colors[1]++;
+          if(thePlayers.get(i).getBasket().get(b).getColor().equals("yellow"))
+            colors[2]++;
+          if(thePlayers.get(i).getBasket().get(b).getColor().equals("green"))
+            colors[3]++; 
+      }
+    }
+    
+    System.out.println("\n\nTotal eggs found:");
+    System.out.println("Blue eggs: " + colors[0]);
+    System.out.println("Pink eggs: " + colors[1]);
+    System.out.println("Yellow eggs: " + colors[2]);
+    System.out.println("Green eggs: " + colors[3]);
+  }
+}
---
+++ Player.java
@@ -0,0 +1,49 @@
+/**
+* This class determines the color and contents of the eggs 
+* @author Martin Manevski, Nicholas Warren, Jamie Schaffer, Mariam Bahassi
+* @version 3/14/2021
+*/
+
+import java.util.*;
+
+class Player {
+  private int eggs;
+  private ArrayList<Egg> basket;
+
+//constructor with no parameters
+  Player () {
+    eggs = 0;
+    basket = new ArrayList<Egg>();
+  }
+
+/**
+* this adds an egg to the basket when a player finds an egg 
+* @param Egg and addEgg
+*/
+  public void foundEgg(Egg anEgg) {
+   eggs++;
+   basket.add(anEgg); 
+  }
+
+//prints out the contents of the basket
+  public void printBasket() {
+    for(int i=0; i<basket.size(); i++) {
+      basket.get(i).printEgg();
+    }
+  }
+/**
+* accesses the number of eggs and returns it 
+* @return number of eggs
+*/
+  public int getNumberEggs() {
+    return eggs;
+  }
+
+/**
+* accesses the basket and returns it 
+* @return basket of eggs
+*/
+  public ArrayList<Egg> getBasket() {
+    return basket;
+  }
+}
